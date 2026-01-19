a860732daad96a3a9b7ec59b77e8c801dc8d451e
Jack Kill <jackkillschool@gmail.com>
2026-01-19 05:19:02 -0600

    Attempting a deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), visualizer()],
